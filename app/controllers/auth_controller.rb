91a67b8d77ac91849ea0f0b9909f48e944f14ff5
brvarner <bvarner4592@gmail.com>
2025-07-31 17:36:18 -0500

    completed simulated JWT auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,8 +1,26 @@
 class AuthController < ApplicationController
   def create
-    
+    token = create_token()
+    session[:auth_token] = token
+    render json: token
   end
 
   def show
+    if session[:auth_token]
+      render json: session[:auth_token]
+    else
+      session[:auth_token] = create_token()
+      render json: session[:auth_token]
+    end
+  end
+
+  private
+
+  def create_token
+    random_string = SecureRandom.hex
+    payload = { data: random_string }
+    token = JWT.encode(payload, nil, "none")
+
+    token
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "auth/create"
-  get "auth/show"
   post "/contacts", to: "contacts#create"
   get "/contacts", to: "contacts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
