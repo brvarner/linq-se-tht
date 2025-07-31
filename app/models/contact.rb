bac9a02397ee1662a2ced642212bfa4de9de5501
brvarner <bvarner4592@gmail.com>
2025-07-31 16:41:06 -0500

    updated validations/normalizations in models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   validates :email_address, presence: true, uniqueness: true
   validates_presence_of :first_name, :last_name
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
