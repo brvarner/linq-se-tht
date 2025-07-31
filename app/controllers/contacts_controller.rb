42fc0068f55e0c98834648998ae89cb239f0fd62
brvarner <bvarner4592@gmail.com>
2025-07-31 17:42:13 -0500

    updated the contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,19 @@
 class ContactsController < ApplicationController
   def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      render json: @contact
+    else
+      render :new
+    end
+  end
+
+  def new
+    @contact = Contact.new
   end
 
   def index
+    @contact = Contacts.all
   end
 end
