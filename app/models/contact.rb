a86f2b76c26515270f92e709d4eda1126f142f04
brvarner <bvarner4592@gmail.com>
2025-07-31 16:39:01 -0500

    added User model for interfacing, migrated table

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  validates :email_address, presence: true, uniqueness: true
+  validates_presence_of :first_name, :last_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_31_213541) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_31_213830) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
