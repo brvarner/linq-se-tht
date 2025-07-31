0ee0eb49a85347234f3409d4638a1543d394aad1
brvarner <bvarner4592@gmail.com>
2025-07-31 17:15:31 -0500

    added auth controller and routes to simulate auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get "auth/create"
+  get "auth/show"
   post "/contacts", to: "contacts#create"
   get "/contacts", to: "contacts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  post "auth/login", to: "auth#create"
+  get "auth/login/user", to: "auth#show"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
